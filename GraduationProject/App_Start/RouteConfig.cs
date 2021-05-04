7ea71109475ecc541e79c51a604c97e4628ee703
mohamed-fathy3010 <46897335+mohamed-fathy3010@users.noreply.github.com>
2021-05-04 10:24:00 +0200

    migrating to razor patch 1

--- GraduationProject/App_Start/RouteConfig.cs
+++ GraduationProject/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace GraduationProject
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ GraduationProject/Controllers/SellerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraduationProject.Controllers
+{
+    public class SellerController : Controller
+    {
+        // GET: Seller
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("financials/account-summary")]
+        public ActionResult AccountSummary()
+        {
+            return View("~/Views/Seller/Financials/AccountSummary.cshtml");
+        }
+    }
+}

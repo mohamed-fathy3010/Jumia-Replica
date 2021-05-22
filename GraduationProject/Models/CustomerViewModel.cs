30aa4fbdd6253dd84b8e0975c1be043c13b6e6ac
Basselfathy <basselfathy@gmail.com>
2021-05-22 08:58:49 +0200

    account

---
+++ GraduationProject/Models/CustomerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GraduationProject.Models
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        [Required]
+        public string Fname { get; set; }
+        [Required]
+        public string Lname { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}

4c61f992c052dc25bb87081367fa86f0d4951aac
mohamed-fathy3010 <46897335+mohamed-fathy3010@users.noreply.github.com>
2021-05-25 15:49:16 +0200

    finished search page(is product added to cart) finished account edit with birthday

---
+++ GraduationProject/Models/SearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraduationProject.Models
+{
+    public class SearchViewModel
+    {
+        public Product Product { get; set; }
+        public bool IsAddedToCart { get; set; }
+    }
+}
